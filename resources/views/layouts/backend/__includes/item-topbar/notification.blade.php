8bdf68e35301a38cf56a9fa6b5b041ad30ca1ba2
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2024-02-26 04:03:00 +0800

    U#8

--- resources/views/layouts/backend/__includes/item-topbar/notification.blade.php
+++ resources/views/layouts/backend/__includes/item-topbar/notification.blade.php
@@ -80,29 +80,26 @@
               </div>
             </div>
 
-            <!--begin::Item-->
             <div class="d-flex align-items-center mb-6">
-              <!--begin::Symbol-->
               <div class="symbol symbol-40 symbol-light-success mr-5">
                 <span class="symbol-label">
-                  <span class="svg-icon svg-icon-lg svg-icon-success"><!--begin::Svg Icon | path:/assets/backend/media/svg/icons/Communication/Group-chat.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                  <span class="svg-icon svg-icon-lg svg-icon-success">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                     <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                       <rect x="0" y="0" width="24" height="24"/>
                       <path d="M16,15.6315789 L16,12 C16,10.3431458 14.6568542,9 13,9 L6.16183229,9 L6.16183229,5.52631579 C6.16183229,4.13107011 7.29290239,3 8.68814808,3 L20.4776218,3 C21.8728674,3 23.0039375,4.13107011 23.0039375,5.52631579 L23.0039375,13.1052632 L23.0206157,17.786793 C23.0215995,18.0629336 22.7985408,18.2875874 22.5224001,18.2885711 C22.3891754,18.2890457 22.2612702,18.2363324 22.1670655,18.1421277 L19.6565168,15.6315789 L16,15.6315789 Z" fill="#000000"/>
                       <path d="M1.98505595,18 L1.98505595,13 C1.98505595,11.8954305 2.88048645,11 3.98505595,11 L11.9850559,11 C13.0896254,11 13.9850559,11.8954305 13.9850559,13 L13.9850559,18 C13.9850559,19.1045695 13.0896254,20 11.9850559,20 L4.10078614,20 L2.85693427,21.1905292 C2.65744295,21.3814685 2.34093638,21.3745358 2.14999706,21.1750444 C2.06092565,21.0819836 2.01120804,20.958136 2.01120804,20.8293182 L2.01120804,18.32426 C1.99400175,18.2187196 1.98505595,18.1104045 1.98505595,18 Z M6.5,14 C6.22385763,14 6,14.2238576 6,14.5 C6,14.7761424 6.22385763,15 6.5,15 L11.5,15 C11.7761424,15 12,14.7761424 12,14.5 C12,14.2238576 11.7761424,14 11.5,14 L6.5,14 Z M9.5,16 C9.22385763,16 9,16.2238576 9,16.5 C9,16.7761424 9.22385763,17 9.5,17 L11.5,17 C11.7761424,17 12,16.7761424 12,16.5 C12,16.2238576 11.7761424,16 11.5,16 L9.5,16 Z" fill="#000000" opacity="0.3"/>
                     </g>
-                  </svg><!--end::Svg Icon--></span>                    </span>
+                  </svg>
+                </span>
+              </span>
                 </div>
-                <!--end::Symbol-->
 
-                <!--begin::Text-->
                 <div class="d-flex flex-column font-weight-bold">
                   <a href="#" class="text-dark text-hover-primary mb-1 font-size-lg">Claudy Sys</a>
                   <span class="text-muted">Project Deployment & Launch</span>
                 </div>
-                <!--end::Text-->
               </div>
-              <!--end::Item-->
 
               <!--begin::Item-->
               <div class="d-flex align-items-center mb-6">
@@ -117,22 +114,18 @@
                         <path d="M5.89339828,3.42893219 C6.44568303,3.42893219 6.89339828,3.87664744 6.89339828,4.42893219 L6.89339828,6.42893219 C6.89339828,6.98121694 6.44568303,7.42893219 5.89339828,7.42893219 C5.34111353,7.42893219 4.89339828,6.98121694 4.89339828,6.42893219 L4.89339828,4.42893219 C4.89339828,3.87664744 5.34111353,3.42893219 5.89339828,3.42893219 Z M11.4289322,5.13603897 C11.8194565,5.52656326 11.8194565,6.15972824 11.4289322,6.55025253 L10.0147186,7.96446609 C9.62419433,8.35499039 8.99102936,8.35499039 8.60050506,7.96446609 C8.20998077,7.5739418 8.20998077,6.94077682 8.60050506,6.55025253 L10.0147186,5.13603897 C10.4052429,4.74551468 11.0384079,4.74551468 11.4289322,5.13603897 Z M0.600505063,5.13603897 C0.991029355,4.74551468 1.62419433,4.74551468 2.01471863,5.13603897 L3.42893219,6.55025253 C3.81945648,6.94077682 3.81945648,7.5739418 3.42893219,7.96446609 C3.0384079,8.35499039 2.40524292,8.35499039 2.01471863,7.96446609 L0.600505063,6.55025253 C0.209980772,6.15972824 0.209980772,5.52656326 0.600505063,5.13603897 Z" fill="#000000" opacity="0.3" transform="translate(6.014719, 5.843146) rotate(-45.000000) translate(-6.014719, -5.843146) "/>
                         <path d="M17.9142136,15.4497475 C18.4664983,15.4497475 18.9142136,15.8974627 18.9142136,16.4497475 L18.9142136,18.4497475 C18.9142136,19.0020322 18.4664983,19.4497475 17.9142136,19.4497475 C17.3619288,19.4497475 16.9142136,19.0020322 16.9142136,18.4497475 L16.9142136,16.4497475 C16.9142136,15.8974627 17.3619288,15.4497475 17.9142136,15.4497475 Z M23.4497475,17.1568542 C23.8402718,17.5473785 23.8402718,18.1805435 23.4497475,18.5710678 L22.0355339,19.9852814 C21.6450096,20.3758057 21.0118446,20.3758057 20.6213203,19.9852814 C20.2307961,19.5947571 20.2307961,18.9615921 20.6213203,18.5710678 L22.0355339,17.1568542 C22.4260582,16.76633 23.0592232,16.76633 23.4497475,17.1568542 Z M12.6213203,17.1568542 C13.0118446,16.76633 13.6450096,16.76633 14.0355339,17.1568542 L15.4497475,18.5710678 C15.8402718,18.9615921 15.8402718,19.5947571 15.4497475,19.9852814 C15.0592232,20.3758057 14.4260582,20.3758057 14.0355339,19.9852814 L12.6213203,18.5710678 C12.2307961,18.1805435 12.2307961,17.5473785 12.6213203,17.1568542 Z" fill="#000000" opacity="0.3" transform="translate(18.035534, 17.863961) scale(1, -1) rotate(45.000000) translate(-18.035534, -17.863961) "/>
                       </g>
-                    </svg><!--end::Svg Icon--></span>                    </span>
+                    </svg>
+                  </span>
+                </span>
                   </div>
-                  <!--end::Symbol-->
 
-                  <!--begin::Text-->
                   <div class="d-flex flex-column font-weight-bold">
                     <a href="#" class="text-dark text-hover-primary mb-1 font-size-lg">Trilo Service</a>
                     <span class="text-muted">Analytics & Requirement Study</span>
                   </div>
-                  <!--end::Text-->
                 </div>
-                <!--end::Item-->
 
-                <!--begin::Item-->
                 <div class="d-flex align-items-center mb-6">
-                  <!--begin::Symbol-->
                   <div class="symbol symbol-40 symbol-light-info mr-5">
                     <span class="symbol-label">
                       <span class="svg-icon svg-icon-lg  svg-icon-info"><!--begin::Svg Icon | path:/assets/backend/media/svg/icons/Communication/Shield-user.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -144,20 +137,14 @@
                         </g>
                       </svg><!--end::Svg Icon--></span>                    </span>
                     </div>
-                    <!--end::Symbol-->
 
-                    <!--begin::Text-->
                     <div class="d-flex flex-column font-weight-bold">
                       <a href="#" class="text-dark text-hover-primary mb-1 font-size-lg">Bravia SAAS</a>
                       <span class="text-muted">Reporting Application</span>
                     </div>
-                    <!--end::Text-->
                   </div>
-                  <!--end::Item-->
 
-                  <!--begin::Item-->
                   <div class="d-flex align-items-center mb-6">
-                    <!--begin::Symbol-->
                     <div class="symbol symbol-40 symbol-light-danger mr-5">
                       <span class="symbol-label">
                         <span class="svg-icon svg-icon-lg  svg-icon-danger"><!--begin::Svg Icon | path:/assets/backend/media/svg/icons/Communication/Mail-notification.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -168,20 +155,14 @@
                           </g>
                         </svg><!--end::Svg Icon--></span>                    </span>
                       </div>
-                      <!--end::Symbol-->
 
-                      <!--begin::Text-->
                       <div class="d-flex flex-column font-weight-bold">
                         <a href="#" class="text-dark text-hover-primary mb-1 font-size-lg">Express Wind</a>
                         <span class="text-muted">Software Analytics & Design</span>
                       </div>
-                      <!--end::Text-->
                     </div>
-                    <!--end::Item-->
 
-                    <!--begin::Item-->
                     <div class="d-flex align-items-center mb-6">
-                      <!--begin::Symbol-->
                       <div class="symbol symbol-40 symbol-light-success mr-5">
                         <span class="symbol-label">
                           <span class="svg-icon svg-icon-lg  svg-icon-success"><!--begin::Svg Icon | path:/assets/backend/media/svg/icons/Design/Bucket.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -192,26 +173,17 @@
                             </g>
                           </svg><!--end::Svg Icon--></span>                    </span>
                         </div>
-                        <!--end::Symbol-->
 
-                        <!--begin::Text-->
                         <div class="d-flex flex-column font-weight-bold">
                           <a href="#" class="text-dark text-hover-primary mb-1 font-size-lg">Bruk Fitness</a>
                           <span class="text-muted">Web Design & Development</span>
                         </div>
-                        <!--end::Text-->
                       </div>
-                      <!--end::Item-->
                     </div>
-                    <!--end::Scroll-->
 
-                    <!--begin::Action-->
                     <div class="d-flex flex-center pt-7"><a href="#" class="btn btn-light-primary font-weight-bold text-center">See All</a></div>
-                    <!--end::Action-->
                   </div>
-                  <!--end::Tabpane-->
 
-                  <!--begin::Tabpane-->
                   <div class="tab-pane" id="topbar_notifications_events" role="tabpanel">
                     <!--begin::Nav-->
                     <div class="navi navi-hover scroll my-4" data-scroll="true" data-height="300" data-mobile-height="200">
