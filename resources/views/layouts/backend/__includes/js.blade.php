ab7075cd60404f392ff72f88f72cdb6560d8fc33
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2024-03-06 10:25:33 +0700

    U#13

--- resources/views/layouts/backend/__includes/js.blade.php
+++ resources/views/layouts/backend/__includes/js.blade.php
@@ -3,3 +3,31 @@
 <script src="/assets/backend/js/scripts.bundle.js?v=7.0.6"></script>
 
 @stack('js')
+
+<script>
+$("#logout_topbar").click(function(e) {
+  Swal.fire({
+    title: "{{ __('default.notification.confirm.are-you-sure') }}?",
+    text: "{{ __('default.label.logout-session') }}",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: '{{ __("default.label.yes") }}',
+    cancelButtonText: '{{ __("default.label.no") }}',
+    reverseButtons: false
+  }).then(function(result) {
+    if (result.value) {
+      Swal.fire({
+        text: "{{ __('default.label.redirect-login') }}",
+        timer: 2000,
+        onOpen: function() {
+          Swal.showLoading()
+        }
+      }).then(function(result) {
+        if (result.dismiss === "timer") {
+          window.location = "{{ url('/dashboard/logout') }}";
+        }
+      })
+    }
+  });
+});
+</script>
